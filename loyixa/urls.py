71288a15034bddad62a2e20aaeaf982c3f76e62d
Fathulla Latipov <fathullaxonlatipov@gmail.com>
2024-07-15 21:34:29 +0500

    asf

--- loyixa/urls.py
+++ loyixa/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('zapchast/', Zapchast_all.as_view()),
     path('maxsulot/', Maxsulot_all.as_view()),
     path('order/', OrdersView.as_view()),
-    path('orders/', OrderListCreate.as_view(), name='order-list-create'),
+    # path('orders/', OrderListCreate.as_view(), name='order-list-create'),
 ]
